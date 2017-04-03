31ac12bbb3be0c0b9212aa8e1437c7d62946b99c
brianhouse <brian.house@gmail.com>
2017-04-03 13:19:39 -0400

    now clicking

--- lonelyghosts.ino
+++ lonelyghosts.ino
@@ -4,9 +4,9 @@
 
 String idString = "1";
 
-const char* ssid     = "treehouse";
-const char* password = "10happysquirrels";
-const char* host     = "192.168.1.4";
+const char* ssid     = "3V8VC";
+const char* password = "7YYGM8V3R65V52FJ";
+const char* host     = "192.168.1.15";
 
 WiFiUDP Udp;
 const int port       = 23232; // both send and receive
@@ -21,12 +21,38 @@ int lit = 0;
 const float Pi = 3.141593;
 
 void setup() {
-//  tone(8, 4, 4);
   Serial.begin(115200);
   delay(10);
-  pinMode(0, OUTPUT);  
+  pinMode(0, OUTPUT);  // set as output to use red LED (LOW is on, HIGH is off)
+  pinMode(2, OUTPUT);  // set as output to use blue LED (LOW is on, HIGH is off)
+  pinMode(12, OUTPUT); // piezo, using with tone
   connectToWifi();
   Udp.begin(port);
+}
+
+void loop() {
+  if (lit == 10) {
+    digitalWrite(2, LOW);  // on
+    tone(12, 4978, 1);
+    lit--;
+  } else if (lit > 0) {
+    lit--;
+  } else {
+    digitalWrite(2, HIGH);  // off
+  }
+//  if (WiFi.status() != WL_CONNECTED) {    // speed impact?
+//    connectToWifi();   
+//  }
+  int packetSize = Udp.parsePacket();  
+  if (packetSize) {
+    char packetBuffer[packetSize];
+    Udp.read(packetBuffer, packetSize);
+    if (String(packetBuffer) == "bump") {
+      bump();
+    }
+  }
+  increment();
+  delay(10);
 }
 
 void increment() {
@@ -62,27 +88,6 @@ float f(float x) {
 
 float f_inv(float y) {
   return (2/Pi) * asin(y);
-}
-
-void loop() {
-  if (lit == 10) {
-    pinMode(2, HIGH);  
-    lit--;
-  } else if (lit > 0) {
-    lit--;
-  } else {
-    pinMode(2, LOW);
-  }
-  int packetSize = Udp.parsePacket();  
-  if (packetSize) {
-    char packetBuffer[packetSize];
-    Udp.read(packetBuffer, packetSize);
-    if (String(packetBuffer) == "bump") {
-      bump();
-    }
-  }
-  increment();
-  delay(10);
 }
 
 void connectToWifi() {
@@ -132,4 +137,5 @@ void printWifiStatus() {
 
 // maybe need a rest period after firing when cant be bumped
 
-// https://www.arduino.cc/en/Tutorial/ToneMelody?from=Tutorial.Tone
+// https://learn.adafruit.com/adafruit-feather-huzzah-esp8266/pinouts
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import serial, threading, time, os, socket, queue
-from housepy import config, log, util
+from housepy import config, log, timeutil
 
 
 class FeatherListener(threading.Thread):
@@ -31,8 +31,9 @@ class FeatherListener(threading.Thread):
         while True:
             try:
                 message, address = self.socket.recvfrom(1024)
+                ip, port = address
                 data = message.decode('utf-8').split(',')
-                data = {'id': str(address[0]), 'rssi': int(data[1]), 'ip': address, 't_utc': util.timestamp(ms=True), 'data': [v for v in data[2:]]}
+                data = {'id': int(data[0]), 'rssi': int(data[1]), 'ip': ip, 't_utc': timeutil.timestamp(ms=True), 'data': [v for v in data[2:]]}
                 self.messages.put(data)
                 if data['id'] not in self.events:
                     self.events[data['id']] = queue.Queue()
@@ -95,16 +96,16 @@ if __name__ == "__main__":
     fs = FeatherSender()
     feather_ids = []
     def message_handler(response):
-        # log.info("%f [ID %s] [IP %s] [RSSI %d]:\t%s" % (response['t_utc'], response['id'], response['ip'][0], response['rssi'], "".join(["%s " % f for f in response['data']])))
+        log.info("%f [ID %s] [IP %s] [RSSI %d]:\t%s" % (response['t_utc'], response['id'], response['ip'], response['rssi'], "".join(["%s " % f for f in response['data']])))
         if response['id'] not in feather_ids:
             feather_ids.append(response['id'])
         if response['data'][0] == "fire":
-            log.info("%s fire!" % response['id'])
+            # log.info("%s fire!" % response['id'])
             for feather_id in feather_ids:
                 if feather_id == response['id']:
                     continue
                 fs.send("bump", (feather_id, 23232))
-            log.info("")
+            # log.info("")
     fl = FeatherListener(message_handler=message_handler)
     while True:
         time.sleep(1)
